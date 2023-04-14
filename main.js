74668a362b67ed990ca4021adcdb642b3a90bf7c
ovtt17 <ovettmora17@gmail.com>
2023-04-14 00:25:36 -0600

    Now we can type and see the text in the encryption section

---
+++ main.js
@@ -0,0 +1,28 @@
+
+let textContent = document.querySelector('#text-content');
+let encryptButton = document.querySelector('.encrypt-btn');
+let decryptButton = document.querySelector('.decrypt-btn');
+
+//text container
+let textContainer = document.querySelector('.text-container');  
+
+//text img
+let textImage = document.querySelector('.text-img');
+//text image container
+let textImageContainer = document.querySelector('.text-image-container');
+
+//listening buttons
+encryptButton.addEventListener('click', encryptText);
+// decryptButton.addEventListener('click', );
+
+
+let encryptedText = document.createElement('p');
+encryptedText.classList.add('encrypted-text');
+textContainer.appendChild(encryptedText);
+
+function encryptText() {
+  textImage.style.display = 'none';
+  textImageContainer.style.display = 'none';
+  encryptedText.textContent = "";
+  encryptedText.innerText = textContent.value.toLowerCase();
+}
